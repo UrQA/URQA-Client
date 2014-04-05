3df39a93b2c77892434c55d766978fe287f0243d
SeunOh <zeros0317@gmail.com>
2014-04-05 17:02:27 +0900

    프로젝트 이름 변경 및 .gitignore 추가

--- UrQA-Library/src/com/urqa/exceptionhandler/ExceptionHandler.java
+++ UrQA-Library/src/com/urqa/exceptionhandler/ExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.urqa.exceptionhandler;
 
+import android.os.Looper;
+
 import com.urqa.Collector.ErrorReport;
 import com.urqa.Collector.ErrorReportFactory;
 import com.urqa.common.SendErrorProcess;
@@ -8,11 +10,11 @@ import com.urqa.rank.ErrorRank;
 
 public class ExceptionHandler implements Thread.UncaughtExceptionHandler{
 
-	Thread.UncaughtExceptionHandler m_DefaultExceptionHandler;
+	Thread.UncaughtExceptionHandler mUncaughtExceptionHandler;
 	
 	public ExceptionHandler() {
 		// TODO Auto-generated constructor stub
-		m_DefaultExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+		mUncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
         Thread.setDefaultUncaughtExceptionHandler(this);
 	}
 
@@ -24,10 +26,17 @@ public class ExceptionHandler implements Thread.UncaughtExceptionHandler{
 		SendErrorProcess errprocess = new SendErrorProcess(report, "client/send/exception");
 		errprocess.start();
 		
-		m_DefaultExceptionHandler.uncaughtException(thread, ex);
+        /**
+         * Android App 강제 종료 문제
+         */
+        //		m_DefaultExceptionHandler.uncaughtException(thread, ex);
 		
-		android.os.Process.killProcess(android.os.Process.myPid());
-        System.exit(0);
+        //		android.os.Process.killProcess(android.os.Process.myPid());
+        //        System.exit(0);
+        
+        
+        // MainThread 에서 Error 발생시 종료 함.
+        exitMainThreadApplication(thread, ex);
     }
 
 	@Override
@@ -35,5 +44,13 @@ public class ExceptionHandler implements Thread.UncaughtExceptionHandler{
 		// TODO Auto-generated method stub
 		super.finalize();
 	}
+    
+    private void exitMainThreadApplication(Thread thread, Throwable throwable) {
+        if (thread == Looper.getMainLooper().getThread()) {
+            mUncaughtExceptionHandler.uncaughtException(thread, throwable);
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(10);
+        }
+    }
 
 }
